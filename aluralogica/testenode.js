2dee1b2072832cf2e06bbf461e88eb47a4fe1440
ericodc <ericodc@gmail.com>
2022-06-16 21:15:35 -0300

    recomeço do lynda

---
+++ aluralogica/testenode.js
@@ -0,0 +1,12 @@
+function pulaLinha() {
+  // document.write('<br>')
+  console.log()
+}
+
+function mostra(frase) {
+  document.write(frase)
+}
+
+var ngeracoes = Math.round((2022 - 1500) / 28)
+
+print('desde o descobrimento passaram ' + ngeracoes + ' gerações')
